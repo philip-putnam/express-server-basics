19f2ca7457b7b332cb2a2c73c6a74deda07f7cf4
Philip Putnam <putnamp989@gmail.com>
2017-06-18 18:08:35 -0700

    add /blog route and sample data in data folder

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 'use strict';
 
 var express = require('express');
+var posts = require('./data/posts.json');
 
 var app = express();
 
 app.get('/', (req, res) => {
   res.send("<h1>I love learning how to code!</h1>")
+});
+
+app.get('/blog', (req, res) => {
+
 });
 
 app.listen(3000, () => {
