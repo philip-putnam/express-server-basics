a61f89016aa134555c554bbb2a4a09ed08803e5b
Philip Putnam <putnamp989@gmail.com>
2017-06-18 17:54:16 -0700

    add root route '/' with some temp content

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
 var express = require('express');
+
+var app = express();
+
+app.get('/', (request, response) => {
+  response.send("I love learning how to code!")
+});
+
+app.listen(3000);
